5051e06903acb256685d46fe64ae3cef79dc181c
glory <glory@peteasy.kr>
2022-05-24 23:33:48 +0900

    빈시간판매하기v2

--- data/get_book_data_ajax.php
+++ data/get_book_data_ajax.php
@@ -1195,39 +1195,59 @@ switch($clear['mode']){
     //일 단위 빈시간 판매 데이터 업데이트
     case 'awaitAddDay':
         //빈시간 판매 데이터를 삭제한다.
-//        print_r($_POST);
-        for($k=0;$k<count($_POST['data']);$k++) {
-            $split = explode(",",$_POST['data'][$k]);
-            print_r($split);
-            $que = "SELECT * FROM tb_sale_free_time_temp WHERE artist_id = '{$_POST['artist_id']}' AND worker = '{$split[0]}'";
-            echo $que;
-            $rows = sql_fetch_array($que);
-            if(!$rows[0]['artist_id']){
-                $sql  = "INSERT INTO tb_sale_free_time_temp SET ";
-                $sql .= "artist_id  = '{$_POST['artist_id']}', ";
-                $sql .= "worker     = '{$split[0]}', ";
-                $sql .= "empty_date = '{$split[1]}|', ";
-                $sql .= "reg_dt     = NOW() ";
-                echo $sql;
+        //print_r($_POST);
+        $sel_dt = trim($_POST['sel_date']);
+        $ex = explode('.',$sel_dt);
+        $year = $ex[0];
+        $month = sprintf('%02d',$ex[1]);
+        $day = sprintf('%02d',$ex[2]);
+        $sel_dt = $year.'-'.$month.'-'.$day;
+        //$sel_dt = str_replace(".","-",$_POST['sel_date']);
+        $worker = array();
+        $dt = array();
+        $dtworker = array();
+        for($i=0;$i<count($_POST['data']);$i++){
+            $worker = explode(",",$_POST['data'][$i]);
+            $dt[$worker[0]][] = $worker[1];
+            $dtworker[] = $worker[0];
+            $dtdate[] = substr($worker[1],0,10);
+            $dttime[] = substr($worker[1],-5);
+        }
+
+        $que = "DELETE FROM tb_sale_free_time_temp WHERE artist_id = '{$_POST['artist_id']}' AND empty_date = '{$sel_dt}'";
+        //echo $que;
+        sql_query($que);
+        for($i=0;$i<count($dtworker);$i++) {
+            $que = "SELECT COUNT(*) AS cnt FROM tb_sale_free_time_temp WHERE artist_id = '{$_POST['artist_id']}' AND worker = '{$dtworker[$i]}' AND empty_date = '{$dtdate[$i]}' AND empty_datetime = '{$dttime[$i]}' ";
+            //echo $que."<br>";
+            $row = sql_fetch_array($que);
+            if(!$row[0]['cnt']){
+                $sql = "INSERT INTO tb_sale_free_time_temp SET ";
+                $sql .= "artist_id          = '{$_POST['artist_id']}', ";
+                $sql .= "worker             = '{$dtworker[$i]}', ";
+                $sql .= "empty_date         = '{$dtdate[$i]}', ";
+                $sql .= "empty_datetime     = '{$dttime[$i]}', ";
+                $sql .= "reg_dt             = NOW() ";
+                //echo $sql;
                 sql_query($sql);
-            } else {
-                $tmp = explode("|",$rows[0]['empty_date']);
-                //print_r($tmp);
-                for($i=0;$i<count($split[1]);$i++){
-                    if(!in_array($split[$k][$i],$tmp)){
-                        array_push($tmp, $_POST['data'][$i]);
-                    }
-                }
-                $json['cnt'] = count($tmp)-1;
-                $arr = implode("|",$tmp);
-                $sql = "UPDATE tb_sale_free_time_temp SET ";
-                $sql .= "empty_date = '{$arr}', ";
-                $sql .= "reg_dt     = NOW() ";
-                $sql .= " WHERE artist_id = '{$_POST['artist_id']}' AND worker = '{$split[$k]}' ";
-                echo $sql;
-                //sql_query($sql);
             }
         }
+
+        $total_cnt = 0;
+        $que = "SELECT * FROM tb_sale_free_time_temp WHERE artist_id = '{$_POST['artist_id']}'";
+        $sftt = sql_fetch_array($que);
+        if(count($sftt)>0){
+            foreach($sftt as $rs){
+                $tmp = explode('|',$rs['empty_date']);
+                for($i=0;$i<count($tmp);$i++) {
+                    if(!empty($tmp[$i])) {
+                        $ed[$rs['worker']][] = $tmp[$i];
+                        $total_cnt++;
+                    }
+                }
+            }
+        }
+        $json['cnt'] = $total_cnt;
         echo json_encode($json);
         break;
 
