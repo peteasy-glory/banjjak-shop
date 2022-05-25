f401247ce124dfc34572c6b2656eead74d597dc3
eaden <eaden@peteasy.kr>
2022-05-26 08:43:10 +0900

    .gitignore TAwsS2.php

--- common/TAwsS3.php
+++ common/TAwsS3.php
@@ -1,4 +1,5 @@
 <?php
+require '/Users/banjjak_dev/Desktop/Dev_Project/2.Dev/3.Web/www/html/subdomain/banjjak_sol/aws/aws-autoloader.php';
 //require '/var/www/html/subdomain/banjjak_sol/vendor/autoload.php';
 
 use Aws\S3\S3Client;
