54595868981bb9f9571257d24884ca878d564688
glory <glory@peteasy.kr>
2022-10-05 19:44:49 +0900

    cron 수정

--- cron/payment_cancel_without_reserve_money.php
+++ cron/payment_cancel_without_reserve_money.php
@@ -4,11 +4,11 @@ include($_SERVER['DOCUMENT_ROOT'] . "/include/global.php");
 $que = "
     UPDATE tb_payment_log SET
     is_cancel = 1,
-    cancel_time = NOW()
+    cancel_time = DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s')
     WHERE is_reserve_pay = 1
     AND payment_log_seq > 600000 #부하 덜기 위함
     AND reserve_pay_yn = 0
-    AND reserve_pay_deadline < NOW()
+    AND reserve_pay_deadline< DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s')
 ";
 $result = mysqli_query($connection,$que);
 
