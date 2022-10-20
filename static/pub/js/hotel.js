63582e07433660e4cd70079ad613e81287b10d14
rolldeep2 <lyw960820@naver.com>
2022-10-20 18:20:16 +0900

    수정

--- static/pub/js/hotel.js
+++ static/pub/js/hotel.js
@@ -2,11 +2,7 @@ function get_hotel_product(id){
 
     return new Promise(function(resolve){
 
-
-
-
         $.ajax({
-
             url:'/data/api.php',
             type:'post',
             data:{
@@ -22,8 +18,6 @@ function get_hotel_product(id){
                 } else if (head.code === 200) {
                     console.log(body)
 
-
-
                     if(body.cat.length === 0 && body.coupon.length ===0 && body.dog.length === 0){
 
                         document.getElementById('hotel_product_none_data').style.display = 'flex';
@@ -33,40 +27,23 @@ function get_hotel_product(id){
 
                     if(body.dog.length === 0 ){
 
-
                         document.getElementById('hotel_dog_product_none_data').style.display = 'flex';
-
-
 
                     }else{
                         document.getElementById('hotel_dog_product_none_data').style.display = 'none';
 
-
                         let set = new Set();
-
 
                         body.dog.forEach(function(v){
 
                             document.getElementById('hotel_dog_colgroup').innerHTML += `<col style="width:auto;">`
                             document.getElementById('hotel_dog_room_list').innerHTML += `<th>${v.room_name}</th>`
 
-
-
                             v.fee_list.forEach(function(v2){
 
                                 set.add(v2.kg);
+
                             })
-
-
-
-                            // v.fee_list.forEach(function(v2){
-                            //
-                            //     document.getElementById('hotel_dog_product_tbody').innerHTML += `<tr><th>${v2.kg}</th></tr>`;
-                            // })
-
-
-
-
 
                         })
 
@@ -92,58 +69,39 @@ function get_hotel_product(id){
 
 function get_hotel_product_dog(data){
 
-
     let kg_list = new Set();
-
-
 
     data.dog.forEach(function(d){
 
         d.fee_list.forEach(function(f){
 
-
             kg_list.add(f.kg);
+
         })
     })
-
-
-
     Array.from(document.getElementById('hotel_dog_product_tbody').children).forEach(function(c){
-
 
         data.dog.forEach(function(d){
 
             let d_kg_list = [];
+
             d.fee_list.forEach(function(f){
-
                 d_kg_list.push(f.kg);
-
-
-
             })
 
             let diff = Array.from(kg_list).filter(x => !d_kg_list.includes(x));
 
-
             d.fee_list.forEach(function(f){
-
                 if(c.children[0].innerText === f.kg){
-
                     c.innerHTML += `<td>${f.normal_price/1000}</td>`
                 }
-
-
             })
 
             if(diff.includes(c.children[0].innerText)){
-
                 c.innerHTML += `<td></td>`
             }
 
         })
-
-
-
     })
 
 
